f24ddbbbfd66225248c6c819d99d1f3d1d504c8b
GayCookie <contact@gaycookie.dev>
2021-08-21 01:53:49 +0200

    0.0.15

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+const MinecraftAddonBuilder = require("minecraft-addon-toolchain/v1");
+
+const builder = new MinecraftAddonBuilder("ResourcefulGeodes");
+
+module.exports = builder.configureEverythingForMe();
